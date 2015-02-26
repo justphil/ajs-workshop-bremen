2e6281dbf0bb5f74d3d8463d8288a3dcee6415ff
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2015-02-26 11:07:01 +0100

    add colorPicker example with function passing

--- app/scripts/controllers/ColorPickerCtrl.js
+++ app/scripts/controllers/ColorPickerCtrl.js
@@ -1,14 +1,8 @@
 angular.module('mevisApp').controller('ColorPickerCtrl', function($scope) {
   console.log('ColorPickerCtrl scope ->', $scope);
 
-  $scope.r = 10;
-  $scope.g = 10;
-  $scope.b = 10;
-
-  $scope.increase = function() {
-    $scope.r += 10;
-    $scope.g += 10;
-    $scope.b += 10;
+  $scope.onColorChange = function(r, g, b) {
+    console.log('color has changed', r, g, b);
   };
 
 });
--- app/scripts/directives/colorPicker.js
+++ app/scripts/directives/colorPicker.js
@@ -5,12 +5,25 @@ angular.module('mevisApp').directive('colorPicker', function() {
     restrict: 'E', // E = element / tag
     templateUrl: 'templates/directives/colorPickerTemplate.html',
     scope: {
-      rot: '=initR',
-      gruen: '=initG',
-      blau: '=initB'
+      rot: '@initR',
+      gruen: '@initG',
+      blau: '@initB',
+      onColorChangeFn: '&onColorChangeAttribute'
     },
-    link: function() {
-      console.log('colorPicker instance created!');
+    link: function(scope) {
+
+      ['rot', 'gruen', 'blau'].forEach(function(c) {
+        scope.$watch(c, function(newValue, oldValue) {
+          if (newValue != oldValue) {
+            scope.onColorChangeFn({
+              r : scope.rot,
+              g : scope.gruen,
+              b : scope.blau
+            });
+          }
+        });
+      });
+
     }
   };
 
